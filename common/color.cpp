d8d81893badea2b75beb60fca1a1100052472f4c
Jonathan Giroux <giroux.jo@gmail.com>
2017-02-04 06:53:37 +0100

    Enhance color related functions

--- common/color.cpp
+++ common/color.cpp
@@ -3,67 +3,64 @@
 #include <algorithm>
 #include <cmath>
 
-static float clamp(float x, float min, float max)
+template <typename T>
+static T clamp(T x, T min, T max)
 {
 	return std::min(max, std::max(min, x));
 }
 
+template <typename T>
+static T wrap(T value, T max)
+{
+	return value - max * std::floor(value / max);
+}
+
 void HSVtoRGB(float H, float S, float V, float &R, float &G, float &B)
 {
-	float Hval = std::fmod(H * 6.0f, 6.0f);
+	float Hval = wrap(H * 6.0f, 6.0f);
 	int sel = (int)Hval;
 	float mod = Hval - sel;
 	float v1 = V * (1.0f - S);
 	float v2 = V * (1.0f - S * mod);
 	float v3 = V * (1.0f - S * (1.0f - mod));
-	switch (sel + 1)
+	switch (sel % 6)
 	{
 	case 0:
-		R = V;
-		G = v1;
-		B = v2;
-		break;
-	case 1:
 		R = V;
 		G = v3;
 		B = v1;
 		break;
-	case 2:
+	case 1:
 		R = v2;
 		G = V;
 		B = v1;
 		break;
-	case 3:
+	case 2:
 		R = v1;
 		G = V;
 		B = v3;
 		break;
-	case 4:
+	case 3:
 		R = v1;
 		G = v2;
 		B = V;
 		break;
-	case 5:
+	case 4:
 		R = v3;
 		G = v1;
 		B = V;
 		break;
-	case 6:
+	case 5:
 		R = V;
 		G = v1;
 		B = v2;
-		break;
-	case 7:
-		R = V;
-		G = v3;
-		B = v1;
 		break;
 	}
 }
 
 uint32_t RGBtoColor(float R, float G, float B)
 {
-	return ((int)(clamp(R, 0.0f, 1.0f) * 255) << 16) |
-		((int)(clamp(G, 0.0f, 1.0f) * 255) << 8) |
-		((int)(clamp(B, 0.0f, 1.0f) * 255) << 0);
+	return (clamp((uint32_t)(R * 256.f), 0u, 255u) << 16) |
+		(clamp((uint32_t)(G * 256.f), 0u, 255u) << 8) |
+		(clamp((uint32_t)(B * 256.f), 0u, 255u) << 0);
 }
